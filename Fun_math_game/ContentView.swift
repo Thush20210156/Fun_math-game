2cf2924560a62f61dd1ebc4177c700ddca3ee62c
Thushini Abeysuriya <thushiniabeysuriya@gmail.com>
2024-10-14 16:44:59 +0530

    Initial Commit

---
+++ Fun_math_game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fun_math_game
+//
+//  Created by Thushini Abeysuriya on 2024-10-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fun_math_game/Fun_math_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fun_math_gameApp.swift
+//  Fun_math_game
+//
+//  Created by Thushini Abeysuriya on 2024-10-14.
+//
+
+import SwiftUI
+
+@main
+struct Fun_math_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
